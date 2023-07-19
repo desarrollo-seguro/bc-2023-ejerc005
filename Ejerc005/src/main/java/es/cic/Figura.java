54bbe10bf17c946a3f9e0ed0ea8a4d139add5ac6
DjHumildeDev <choches61@gmail.com>
2023-07-19 11:46:50 +0200

    Interfaz Figura, clase cuadrilatero

---
+++ Ejerc005/src/main/java/es/cic/Figura.java
@@ -0,0 +1,9 @@
+package es.cic;
+
+public interface Figura {
+    public void aniadirFigura();
+    public void eliminarFigura();
+    public void modificarFIgura();
+    public void moverFigura();   
+    public boolean comprobarPosicion();
+}
