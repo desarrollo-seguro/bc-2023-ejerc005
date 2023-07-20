5834f8f236317f9921502de723e84262005e3666
DjHumildeDev <choches61@gmail.com>
2023-07-20 09:32:33 +0200

    testLienzo

--- Ejerc005/src/test/java/es/cic/lienzoTest.java
+++ Ejerc005/src/test/java/es/cic/lienzoTest.java
@@ -1,10 +1,12 @@
 package es.cic;
 
 import static org.mockito.Mockito.mock;
-
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,22 +36,25 @@ public class lienzoTest {
 
     @Test
     void testEliminarFigura() {
-        
+        cut.CrearFigura(new CirculoImpl(5,"rojo",new Posicion(5,4)), FiguraEnum.Circulo);
+        cut.eliminarFigura(0);
+        assertThat("Se ha eliminado correctamente la figuta", Integer.valueOf(cut.getCirculosLienzo().size()),is(0));
     }
 
     @Test
     void testEstaDentro() {
-
+        
+        assertTrue("Esta dentro", cut.estaDentro(new Posicion(8, 10000)));
     }
 
-    @Test
-    void testGetCirculosLienzo() {
-
-    }
 
     @Test
     void testModificarPosFigura() {
-
+        dependencia.setColor("rojo");
+        dependencia.setRadio(0);
+        dependencia.setPos(new Posicion(5,4));
+        CirculoImpl circuloEsperado= new CirculoImpl(500,"rojo",new Posicion(5,4));
+        when(dependencia.modificarFigura(500)).thenReturn(circuloEsperado);
     }
 
     @Test
